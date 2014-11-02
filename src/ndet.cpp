e30b6b907978ea18b0ce95538a6373d2b1607635
pat42410 <pat42410@hotmail.fr>
2014-11-02 17:58:29 +0100

    maj

--- src/ndet.cpp
+++ src/ndet.cpp
@@ -60,99 +60,99 @@ bool FromFileJff(sAutoNDE& at, string path);
 
 bool FromFile(sAutoNDE& at, string path){
 
-  string extension;
-  if (path.find_last_of(".") != std::string::npos)
-    extension = path.substr(path.find_last_of(".")+1);
-  else
-    extension = "";
+	string extension;
+	if (path.find_last_of(".") != std::string::npos)
+		extension = path.substr(path.find_last_of(".")+1);
+	else
+		extension = "";
 
-  if (extension == "txt")
-    return FromFileTxt(at,path);
-  if (extension == "jff")
-    return FromFileJff(at,path);
+	if (extension == "txt")
+		return FromFileTxt(at,path);
+	if (extension == "jff")
+		return FromFileJff(at,path);
 
-  cout << "extension de fichier non reconnue\n";
-  return false;
+	cout << "extension de fichier non reconnue\n";
+	return false;
 }
 
 bool FromFileTxt(sAutoNDE& at, string path){
+	
+	ifstream myfile(path.c_str(), ios::in); 
+	//un flux d'entree obtenu à partir du nom du fichier
+	string line;
+	// un ligne lue dans le fichier avec getline(myfile,line);
+	istringstream iss;
+	// flux associé à la chaine, pour lire morceau par morceau avec >> (comme cin)
+	etat_t s(0), t(0);
+	// deux états temporaires
+	symb_t a(0);
+	// un symbole temporaire
 
-  ifstream myfile(path.c_str(), ios::in); 
-  //un flux d'entree obtenu à partir du nom du fichier
-  string line;
-  // un ligne lue dans le fichier avec getline(myfile,line);
-  istringstream iss;
-  // flux associé à la chaine, pour lire morceau par morceau avec >> (comme cin)
-  etat_t s(0), t(0);
-  // deux états temporaires
-  symb_t a(0);
-  // un symbole temporaire
+	if (myfile.is_open()){
+		// la première ligne donne 'nb_etats nb_symbs nb_finaux'
+		do{ 
+			getline(myfile,line);
+		} while (line.empty() || line[0]=='#');
+		// on autorise les lignes de commentaires : celles qui commencent par '#'
+		iss.str(line);
+		if((iss >> at.nb_etats).fail() || (iss >> at.nb_symbs).fail() || (iss >> at.nb_finaux).fail())
+			return false;
+		// la deuxième ligne donne l'état initial
+		do{ 
+			getline (myfile,line);
+		} while (line.empty() || line[0]=='#');    
+		iss.clear();
+		iss.str(line);
+		if((iss >> at.initial).fail())
+			return -1;
 
-  if (myfile.is_open()){
-    // la première ligne donne 'nb_etats nb_symbs nb_finaux'
-    do{ 
-      getline(myfile,line);
-    } while (line.empty() || line[0]=='#');
-    // on autorise les lignes de commentaires : celles qui commencent par '#'
-    iss.str(line);
-    if((iss >> at.nb_etats).fail() || (iss >> at.nb_symbs).fail() || (iss >> at.nb_finaux).fail())
-        return false;
-    // la deuxième ligne donne l'état initial
-    do{ 
-      getline (myfile,line);
-    } while (line.empty() || line[0]=='#');    
-    iss.clear();
-    iss.str(line);
-    if((iss >> at.initial).fail())
-      return -1;
-    
-    // les autres lignes donnent les états finaux
-    for(size_t i = 0; i < at.nb_finaux; i++){
-        do{ 
-          getline (myfile,line);
-        } while (line.empty() || line[0]=='#');
-        iss.clear();
-        iss.str(line);
-         if((iss >> s).fail())
-          continue;
-//        cerr << "s= " << s << endl;
-        at.finaux.insert(s);
-    }     
+		// les autres lignes donnent les états finaux
+		for(size_t i = 0; i < at.nb_finaux; i++){
+			do{ 
+				getline (myfile,line);
+			} while (line.empty() || line[0]=='#');
+			iss.clear();
+			iss.str(line);
+			if((iss >> s).fail())
+				continue;
+			//        cerr << "s= " << s << endl;
+			at.finaux.insert(s);
+		}     
 
-    // on alloue les vectors à la taille connue à l'avance pour éviter les resize dynamiques
-    at.epsilon.resize(at.nb_etats);
-    at.trans.resize(at.nb_etats);
-    for(size_t i=0;i<at.nb_etats;++i)
-      at.trans[i].resize(at.nb_symbs);   
+		// on alloue les vectors à la taille connue à l'avance pour éviter les resize dynamiques
+		at.epsilon.resize(at.nb_etats);
+		at.trans.resize(at.nb_etats);
+		for(size_t i=0;i<at.nb_etats;++i)
+			at.trans[i].resize(at.nb_symbs);   
 
-  // lecture de la relation de transition 
-    while(myfile.good()){
-      line.clear();
-      getline (myfile,line);
-      if (line.empty() && line[0]=='#')
-        continue;
-      iss.clear();
-      iss.str(line); 
+		// lecture de la relation de transition 
+		while(myfile.good()){
+			line.clear();
+			getline (myfile,line);
+			if (line.empty() && line[0]=='#')
+				continue;
+			iss.clear();
+			iss.str(line); 
 
-      // si une des trois lectures echoue, on passe à la suite
-      if((iss >> s).fail() || (iss >> a).fail() || (iss >> t).fail() || (a< ASCII_A ) || (a> ASCII_Z ))
-        continue; 
-              
-      //test espilon ou non
-      if ((a-ASCII_A) >= at.nb_symbs){
-        cerr << "s=" << s<< ", (e), t=" << t << endl;
-        //TODO remplir epsilon
-      }
-      else{
-        cerr << "s=" << s<< ", a=" << a-ASCII_A << ", t=" << t << endl;
-        //TODO remplir trans
-      }
-    }
-    myfile.close();
-    return true; 
- }
-  return false;
-  // on ne peut pas ouvrir le fichier
+			// si une des trois lectures echoue, on passe à la suite
+			if((iss >> s).fail() || (iss >> a).fail() || (iss >> t).fail() || (a< ASCII_A ) || (a> ASCII_Z ))
+				continue; 
+
+			//test espilon ou non
+			if ((a-ASCII_A) >= at.nb_symbs){
+				cerr << "s=" << s<< ", (e), t=" << t << endl;
+				//TODO remplir epsilon
+			}
+			else{
+				cerr << "s=" << s<< ", a=" << a-ASCII_A << ", t=" << t << endl;
+				//TODO remplir trans
+			}
+		}
+		myfile.close();
+		return true; 
+	}
+	return false;
+	// on ne peut pas ouvrir le fichier
 }
 
 
